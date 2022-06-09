c9b7db7096d5438353225774da08b4e5f6c024c5
devMiguelCarrero <98110104+devMiguelCarrero@users.noreply.github.com>
2022-06-09 10:27:17 -0500

    remove | build

--- build/admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element', 'wp-i18n'), 'version' => '7cacea413980735412429b28289841ca');
--- build/script.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element', 'wp-i18n'), 'version' => '65707d86649dccd9b07182bc8d2fab9a');
